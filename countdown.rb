99fbe67ca7c7ecf05295b95b92bae290b4db822e
Ynda Jas <yndajas@gmail.com>
2020-07-04 03:35:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(seconds)
-  seconds_to_go = seconds
-  while seconds_to_go > 0
+  while seconds > 0
     puts "#{seconds_to_go} SECOND(S)!"
-    seconds_to_go -= 1
+    seconds -= 1
   end
   "HAPPY NEW YEAR!"
 end

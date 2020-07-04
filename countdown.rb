07ce13977df35b3c4d8765c1393802749134bcc0
Ynda Jas <yndajas@gmail.com>
2020-07-04 03:35:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  seconds_to_go = seconds
+  while seconds_to_go > 0
+    puts "#{seconds_to_go} SECOND(S)!"
+    seconds_to_go -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  seconds_to_go = seconds
+  while seconds_to_go > 0
+    puts "#{seconds_to_go} SECOND(S)!"
+    seconds_to_go -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
